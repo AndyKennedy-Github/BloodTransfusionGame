db5f30f57c40efd66549a87b5593236e52194fb4
AndyKennedy-Github <61878684+AndyKennedy-Github@users.noreply.github.com>
2020-11-28 13:03:29 -0500

    Created Framework for Audio Manager

---
+++ BTG/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip[] sounds;
+
+    public void PlaySoundOnce(gameobject g, int n)
+    {
+        AudioSource a = g.GetComponent<AudioSource>();
+        a.PlayOneShot(sounds[n], 0.5f);
+    }
+
+    public void PlaySound(gameobject g, int n)
+    {
+        AudioSource a = g.GetComponent<AudioSource>();
+        if(a.isPlaying == false)
+        {
+            a.clip = sounds[n];
+            a.Play();
+        }
+    }
+
+    public void StopSound()
+    {
+        AudioSource a = g.GetComponent<AudioSource>();
+        a.Stop();
+    }
+}
