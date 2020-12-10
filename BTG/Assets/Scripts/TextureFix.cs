248f05411f4ef256cb2ebd5db3a502b5cc978a2e
AndyKennedy-Github <61878684+AndyKennedy-Github@users.noreply.github.com>
2020-12-10 09:36:01 -0500

    GameManager has been reimported and is operational

---
+++ BTG/Assets/Scripts/TextureFix.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureFix : MonoBehaviour
+{
+   // Scroll main texture based on time
+
+    float scrollSpeed = 0.5f;
+    Renderer rend;
+    public GameObject scaleObject;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer> ();
+    }
+
+    void Update()
+    {
+        Vector3 s;
+
+        s = scaleObject.transform.localScale;
+
+
+        // Animates main texture scale in a funky way!
+        //float scaleX = Mathf.Cos(Time.time) * 0.5f + 1;
+        //float scaleY = Mathf.Sin(Time.time) * 0.5f + 1;
+        float scaleX = s.x;
+        float scaleY = s.y;
+        rend.material.SetTextureScale("_MainTex", new Vector2(scaleX, scaleY));
+    }
+}
